5c7e4e650750a3440681c470c348250ce05d4099
Chad Williams <52967224+williamschad@users.noreply.github.com>
2019-10-30 12:36:21 +0200

    Update card.js

--- src/components/card.js
+++ src/components/card.js
@@ -1,9 +1,7 @@
-import React, {Component} from "react";
+import React, { Component} from 'react';
 import ReactCardFlip from 'react-card-flip';
-
-import Image from '../components/uno.png';
-
-
+import Image from '../Components/image2.png';
+//Create Card component
 class Card1 extends React.Component {
   constructor() {
     super();
@@ -12,37 +10,36 @@ class Card1 extends React.Component {
     };
     this.handleClick = this.handleClick.bind(this);
   }
-  
-  end = () => {
-    var ab = prompt('1.Restart \n2.Quit');
-    if(ab ==1){
-      window.location.reload()
-    }
-    else if(ab ==2){
-      window.location.href = 'http://localhost:3000/';
-    }
-    
+//function to restart or quit game 
+   end = () => {
+  	var ab = prompt("1.Restart \n2.Quit");
+  	if(ab == 1){
+  		window.location.reload()
+  	}
+  	else if(ab ==2){
+  		window.location.href = "http://localhost:3000/";
+  	}
+
   }
  
   handleClick(e) {
     e.preventDefault();
     this.setState(prevState => ({ isFlipped: !prevState.isFlipped }));
+    setTimeout(this.end, 500)
   }
- 
+//render the component
   render() {
     return (
       <ReactCardFlip isFlipped={this.state.isFlipped} flipDirection="vertical">
-        <div key="front">
-          <img src={Image} alt="An image"/> 
-          <button onClick={this.handleClick}>Click to flip</button>
+        <div key="front" className="b">
+        	<img src={Image} onClick={this.handleClick}/>
         </div>
  
-        <div key="back" className="Aa">
-          {this.props.text}
+        <div key="back" className="b">
+         {this.props.text}
         </div>
       </ReactCardFlip>
     )
   }
 }
-
-export default Card;
+export default Card1;
