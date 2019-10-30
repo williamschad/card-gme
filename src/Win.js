7792c3189b55451f18e95ed0ff8c1496681f5384
Chad Williams <52967224+williamschad@users.noreply.github.com>
2019-10-30 12:35:20 +0200

    Update Win.js

--- src/Win.js
+++ src/Win.js
@@ -1,30 +1,31 @@
 import React from 'react';
+import Card1 from '../Components/Card1';
+import Card2 from '../Components/Card2';
+import Card3 from '../Components/Card3';
 import ReactCardFlip from 'react-card-flip';
-import Container from 'react-bootstrap/Container'
-import Row from 'react-bootstrap/Row'
-import Col from 'react-bootstrap/Col'
-import Card1 from './components/card';
-import Card2 from './components/card2';
-import Card3 from './components/card3';
-
-var myCardText = ['YOU WIN!','YOU LOSE!','BETTER LUCK NEXT TIME!','TRY AGAIN!','SO CLOSE!']
-var randomvalue = myCardText[Math.floor(myCardText.length * Math.random())];
-
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+//Create array to randomize text
+var myCardText = ["YOU WIN!","YOU LOSE!","BETTER LUCK NEXT TIME!","TRY AGAIN!","SO CLOSE!"]
+var randomValue = myCardText[Math.floor(myCardText.length * Math.random())];
+//The Card Component
 const Win = () => {
   return(
     <div>
     <br/>
-    <h1></h1>
-    <h1></h1>
+    <h1>"Life is like a game of cards. The hand you are dealt is determinism; the way you play it is free will."</h1>
+    <h1>PICK A CARD TO WIN!!</h1>
     <br/>
-<Container fluid='false'>
-    <Row>
-      <col><Card1 text={randomValue}/></Col>
-      <col><Card2 text={randomValue}/></Col>
-      <col><Card3 text={randomValue}/></Col>
-    </Row>
+<Container fluid="false">
+  <Row>
+    <Col><Card1 text={randomValue}/></Col>
+    <Col><Card2 text={randomValue}/></Col>
+    <Col><Card3 text={randomValue}/></Col>
+  </Row>
 </Container>
     </div>
     );
-};
+  };
+//exports the Win function
 export default Win;
